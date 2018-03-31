437972bae575640f82da7cb4af3d3d92dfaafa19
ahribori <ahribori@gmail.com>
2018-04-01 01:44:14 +0900

    Practice ng-repeat

--- script.js
+++ script.js
@@ -9,6 +9,22 @@
             title: '텍스트 필드',
             completed: false,
             createdAt: Date.now(),
-        }
+        };
+
+        $scope.dataList = [
+            {
+                index: 0,
+                name: 'ahri',
+            },
+            {
+                index: 1,
+                name: 'bori',
+            },
+            {
+                index: 2,
+                name: 'hi',
+            }
+        ];
+
     }]);
 })();
